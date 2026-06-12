ef6bbd3385d51e364630c1609770150866abcad1
Lesedi Tlhapane <leseditlhapane5@gmail.com>
2026-06-12 14:47:35 +0200

    Perfect update

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AIDiscoveryApp } from "./components/AIDiscoveryApp";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AIDiscoveryApp />
+  </React.StrictMode>
+);
